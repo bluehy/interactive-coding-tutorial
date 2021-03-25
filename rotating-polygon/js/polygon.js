4b0dffd3f673a5ffa977658e3673b52d90616175
hy <chessg55@gmail.com>
2021-03-25 20:29:03 +0900

    colorize rect

--- rotating-polygon/js/polygon.js
+++ rotating-polygon/js/polygon.js
@@ -1,5 +1,23 @@
 const PI2 = Math.PI * 2;
 
+const COLORS = [
+   "#4b45ab",
+   "#554fb8",
+   "#605ac7",
+   "#2a91a8",
+   "#2e9ab2",
+   "#32a5bf",
+   "#81b144",
+   "#85b944",
+   "#8fc549",
+   "#e0af27",
+   "#eeba2a",
+   "#fec72e",
+   "#bf342d",
+   "#ca3931",
+   "#d7423a",
+ ];
+
 export class Polygon {
    constructor(x, y, radius, sides){
       this.x = x;
@@ -40,12 +58,14 @@ export class Polygon {
 
          // 03 꼭짓점 위치의 도형
          ctx.save();
+         ctx.fillStyle = COLORS[i];
          ctx.translate(x, y);
          ctx.rotate(((360 / this.sides) * i + 45) * Math.PI / 180);
          ctx.beginPath();
          for (let j = 0; j < 4; j++){
-            const x2 = 80 * Math.cos(angle2 * j);
-            const y2 = 80 * Math.sin(angle2 * j);
+            // 사각형 만들기
+            const x2 = 100 * Math.cos(angle2 * j);
+            const y2 = 100 * Math.sin(angle2 * j);
             (j == 0) ? ctx.moveTo(x2, y2) : ctx.lineTo(x2, y2);
          }
          ctx.fill();
