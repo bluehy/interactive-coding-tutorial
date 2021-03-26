fd30f423936fa8bd8509c171b66f132beb27566a
hy <chessg55@gmail.com>
2021-03-26 17:27:34 +0900

    new tutorial

---
+++ moving-gradation/js/app.js
@@ -0,0 +1,19 @@
+// app.js
+
+const COLORS = [
+   {r : 45, g : 74, b : 227}, //blue
+   {r : 250, g : 255, b : 89}, //yellow
+   {r : 255, g : 104, b : 248}, //purple
+   {r : 44, g : 209, b : 252}, //skyblue
+   {r : 54, g : 233, b : 84}, //green
+]
+
+class App {
+   constructor() {
+      
+   }
+}
+
+window.onload = () => {
+   new App();
+}
