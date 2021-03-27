8fbb8cc08c9d1052ba9bb3c4e23a0f02fd03e097
hy <chessg55@gmail.com>
2021-03-27 14:53:03 +0900

    pixelRatio

--- moving-gradation/js/app.js
+++ moving-gradation/js/app.js
@@ -10,7 +10,16 @@ const COLORS = [
 
 class App {
    constructor() {
-      
+      this.canvas = document.createElement('canvas');
+      document.body.appendChild(this.canvas);
+      this.ctx = this.canvas.getContext('2d');
+
+      this.pixelRatio = (window.devicePixelRatio > 1) ? 2 : 1 ;
+
+      this.totalParticles = 1;
+      this.particles = [];
+      this.maxRadius = 90;
+      this.minRadius = 40;
    }
 }
 
