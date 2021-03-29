64d1b24964ae722b5ed8882579371369a7f25865
hy <chessg55@gmail.com>
2021-03-29 20:37:39 +0900

    moving - particle

--- moving-gradation/js/app.js
+++ moving-gradation/js/app.js
@@ -63,7 +63,14 @@ class App {
 
 
    animate() {
+      window.requestAnimationFrame(this.animate.bind(this));
 
+      this.ctx.clearRect(0, 0, this.stageWidth, this.stageHeight);
+
+      for (let i = 0; i < this.totalParticles; i++) {
+         const item = this.particles[i];
+         item.animate(this.ctx, this.stageWidth, this.stageHeight);
+      }
    }
    
 }
